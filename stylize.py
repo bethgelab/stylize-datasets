4d2f87fabee5dbfcf5ab08c6cc5a1c5f706baef0
Benjamin <b.mitzkus@gmx.de>
2019-04-30 12:48:40 +0200

    convert content and style to RGB

--- stylize.py
+++ stylize.py
@@ -91,8 +91,8 @@ def main():
     with tqdm(total=len(content_paths) * args.num_styles) as pbar:
         for content_path in content_paths:
             for style_path in random.sample(styles, args.num_styles):
-                content = transform(Image.open(content_path))
-                style = transform(Image.open(style_path))
+                content = transform(Image.open(content_path).convert('RGB'))
+                style = transform(Image.open(style_path).convert('RGB'))
                 style = style.to(device).unsqueeze(0)
                 content = content.to(device).unsqueeze(0)
                 with torch.no_grad():
